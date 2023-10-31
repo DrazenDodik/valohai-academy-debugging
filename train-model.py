16f333416a04bc7b6a0b3e6793ee4e720f4bdf9e
Drazen Dodik <drazend@live.fi>
2023-10-30 17:04:09 -0700

    break precision action

--- train-model.py
+++ train-model.py
@@ -4,9 +4,6 @@ import argparse
 import json
 import debugpy
 
-# Listen on port 5678
-debugpy.listen(5678)
-
 # Valohai: Use argparse to parse the arguments
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -15,21 +12,26 @@ def parse_args():
     parser.add_argument('--debug', type=int, default=0)
     return parser.parse_args()
 
-args = parse_args()
-
-if args.debug is 1:
-    # The script is halted here, until a debugger is attached
-    debugpy.wait_for_client()
-
 # Define a method that prints out key metrics from the trainer
+# yolov8 includes (B) for object detection and (M) for segmentation
 def print_valohai_metrics(trainer):
     metadata = {
         "epoch": trainer.epoch,
-        "mAP50-95(B)": trainer.metrics["metrics/mAP50-95(B)"],
-        "mAP50(B)": trainer.metrics["metrics/mAP50(B)"]
+        "mAP50-95": trainer.metrics["metrics/mAP50-95(B)"],
+        "mAP50": trainer.metrics["metrics/mAP50(B)"],
+        "precision": 0 # TODO: Include precision metric
     }
 
     print(json.dumps(metadata))
+
+args = parse_args()
+
+if args.debug == 1:
+    # Listen on port 5678
+    debugpy.listen(5678)
+
+    # The script is halted here, until a debugger is attached
+    debugpy.wait_for_client()
 
 # Load a model
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
